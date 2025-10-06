e8b29e069e82919e99d36db6e2fa47fa0570d0fa
NagSurendra <nag.subbarayudu@thinkhat.ai>
2025-10-06 13:13:52 +0530

    Update Allure report 2025-10-06

--- update_report.sh
+++ update_report.sh
@@ -4,17 +4,7 @@
 cd /Users/nagasubarayudu/Desktop/IOS || exit
 
 echo "🔗 Setting correct origin remote..."
-git remote remove origin 2>/dev/null
 git remote add origin https://github.com/thinkhatnag/allure-reports.git
-
-echo "📂 Cleaning old docs folder..."
-rm -rf docs
-
-echo "📂 Moving new allure-report to docs..."
-mv allure-report docs
-
-echo "➕ Staging only docs/..."
-git add docs
 
 echo "💾 Committing changes..."
 git commit -m "Update Allure report $(date +'%Y-%m-%d')" || echo "⚠️ Nothing to commit."
